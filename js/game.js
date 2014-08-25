6f81d8c18baa0db40d0328cb06c859204201630e
Wesley Johnson <johnson.wesley.t@gmail.com>
2014-08-24 20:46:08 -0400

    Basically added everything. Forgot to commit on a regular basis.

--- js/game.js
+++ js/game.js
@@ -1,7 +1,11 @@
 var Game = {
   GAME_STATE_START: "start",
   GAME_STATE_PLAYING: "playing",
+  GAME_STATE_LEVEL_COMPLETE: "levelComplete",
+  GAME_STATE_PLAYER_DEAD: "dead",
+  GAME_STATE_LEVEL_STATS: "levelStats",
   GAME_STATE_PAUSED: "paused",
+  GAME_STATE_CREDITS: "credits",
 
   assets: {},
   generateAssets: function() {
@@ -16,123 +20,275 @@ var Game = {
     }
     Game.assets.sky.endDraw();
 
+    // dark sky
+    var topColor = Game.p.color(0, 0, 0);
+    var botColor = Game.p.color(50, 50, 50);
+    Game.assets.darSky = Game.p.createGraphics(40, Game.halfHeight - 40, Game.p.P2D)
+    Game.assets.darSky.beginDraw();
+    for(var i = 0; i < Game.halfHeight - 40; i++) {
+      Game.assets.darSky.stroke(Game.p.lerpColor(topColor, botColor, i / (255 - 40)));
+      Game.assets.darSky.line(0, i, 40, i);
+    }
+    Game.assets.darSky.endDraw();
+
     // hero
-    Game.assets.guy = Game.p.loadImage("assets/images/guy.png");
+    Game.assets.tiles = document.createElement("img");
+    Game.assets.tiles.src = "assets/images/tiles.png";
+
+    // sounds
+    Game.assets.sndJump = new buzz.sound("assets/sounds/jump.mp3");
+    Game.assets.sndTeleport = new buzz.sound("assets/sounds/telport.mp3");
+    Game.assets.sndLevelCompleted = new buzz.sound("assets/sounds/levelcomplete.mp3");
   },
 
-  init: function(processing) {
+  init: function(canvas, context) {
+    Game.canvas = canvas;
+    Game.context = context;
+
+    return Game.initProcessing;
+  },
+
+  initProcessing: function(processing) {
     Game.p = processing;
     Game.ms = Game.p.millis();
     Game.keys = {};
+    Game.worldTime = 0;
 
-    Game.p.setup = function() {
-      Game.p.size(800, 600);
-      Game.p.background(55);
-      Game.gameState = Game.GAME_STATE_START;
-      Game.halfWidth = Game.p.width >> 1;
-      Game.halfHeight = Game.p.height >> 1;
-      Game.generateAssets();
-    };
+    Game.p.setup = Game.setup;
+    Game.p.draw = Game.draw;
+    Game.p.keyPressed = Game.keyPressed;
+    Game.p.keyReleased = Game.keyReleased;
+  },
 
-    Game.p.keyPressed = function() {
-      Game.keys[Game.p.keyCode] = true;
-    };
+  setup: function() {
+    Game.p.size(800, 600);
+    Game.p.background(55);
+    Game.gameState = Game.GAME_STATE_START;
+    Game.halfWidth = Game.p.width >> 1;
+    Game.halfHeight = Game.p.height >> 1;
+    Game.generateAssets();
+    Game.p.textAlign(Game.p.CENTER, Game.p.CENTER);
+  },
 
-    Game.p.keyReleased = function() {
-      Game.keys[Game.p.keyCode] = false;
-    };
+  keyPressed: function() {
+    Game.keys[Game.p.keyCode] = true;
+  },
 
-    Game.anyKey = function() {
-      for(var key in Game.keys) {
-        if(Game.keys.hasOwnProperty(key) && Game.keys[key] == true) {
-          return true;
-        }
+  keyReleased: function() {
+    Game.keys[Game.p.keyCode] = false;
+  },
+
+  anyKey: function() {
+    for(var key in Game.keys) {
+      if(Game.keys.hasOwnProperty(key) && Game.keys[key] == true) {
+        return true;
       }
-
-      return false;
-    };
-
-    Game.clearInput = function() {
-      Game.keys = {};
-    };
-
-    Game.p.update = function(gameTime) {
-      switch(Game.gameState) {
-        case Game.GAME_STATE_START:
-          //if(Game.anyKey()) {
-            Game.loadGame();
-          //}
-          break;
-
-        case Game.GAME_STATE_PAUSED:
-          if(Game.anyKey()) {
-            Game.gameState = Game.GAME_STATE_PLAYING;
-          }
-          break;
-
-        case Game.GAME_STATE_PLAYING:
-          Game.world.update(gameTime);
-          Game.player.update(gameTime);
-          break;
-      }
-    };
-
-    Game.loadGame = function() {
-      Game.clearInput();
-      Game.world = World.loadWorld(0);
-      Game.player = Player.loadPlayer(Game.world.playerStart);
-      Game.gameState = Game.GAME_STATE_PLAYING;
-    };
-
-    Game.clearScreen = function() {
-      Game.p.background(55);
     }
 
-    Game.drawStart = function() {
-      Game.clearScreen();
-      Game.p.textAlign(Game.p.CENTER, Game.p.CENTER);
-      Game.p.fill(Game.p.color(255));
-      Game.p.textSize(40);
-      Game.p.text("CONNECTED WORLDS\n#LD30", Game.halfWidth, Game.halfHeight);
-      Game.p.textSize(18);
+    return false;
+  },
 
-      Game.p.text("Press Any Key", Game.halfWidth, Game.halfHeight + 100);
-      Game.p.noFill();
-      Game.p.stroke(255);
-      Game.p.rect(Game.halfWidth - 100, Game.halfHeight + 100 - 20, 200, 40, 18);
-    };
+  clearInput: function() {
+    Game.keys = {};
+  },
 
-    Game.drawGame = function() {
-      Game.clearScreen();
-      Game.p.image(Game.assets.sky, 0, 0, Game.p.width, Game.halfHeight);
-      Game.p.image(Game.assets.sky, 0, Game.halfHeight, Game.p.width, Game.halfHeight);
-      Game.p.pushMatrix();
-      Game.p.translate(0, 10);
-      Game.world.draw();
-      Game.player.draw();
-      Game.p.popMatrix()
-    };
+  update: function(gameTime) {
+    switch(Game.gameState) {
+      case Game.GAME_STATE_START:
+        if(Game.anyKey()) {
+          Game.loadLevel(worlds.length - 1);
+        }
+        break;
 
-    Game.p.draw = function() {
-      // Calculate gameTime
-      var cms = Game.p.millis();
-      var gameTime = (cms - Game.ms) / 1000;
-      Game.ms = cms;
+      case Game.GAME_STATE_PAUSED:
+        if(Game.anyKey()) {
+          Game.gameState = Game.GAME_STATE_PLAYING;
+        }
+        break;
 
-      // Update
-      Game.p.update(gameTime);
+      case Game.GAME_STATE_PLAYING:
+        Game.worldTime += 1000 * gameTime;
+        Game.world.update(gameTime);
+        Game.player.update(gameTime);
+        if(Game.anyKey()) {
+          Game.notify("key");
+        }
+        break;
 
-      // Render
-      switch(Game.gameState) {
-        case Game.GAME_STATE_START:
-          Game.drawStart();
-          break;
+      case Game.GAME_STATE_LEVEL_COMPLETE:
+        Game.levelCompleteTimer -= 1000 * gameTime;
+        if(Game.levelCompleteTimer <= 0) {
+          Game.levelCompleteTimer = 0;
+          Game.gameState = Game.GAME_STATE_LEVEL_STATS;
+        }
+        break;
 
-        case Game.GAME_STATE_PLAYING:
-        case GAME_STATE_PAUSED:
-          Game.drawGame();
-          break;
+      case Game.GAME_STATE_LEVEL_STATS:
+        if(Game.anyKey()) {
+          if(Game.worldIndex === worlds.length -1) {
+            Game.gameState = Game.GAME_STATE_CREDITS;
+            return;
+          }
+
+          Game.loadLevel(Game.worldIndex + 1);
+        }
+        break;
+    }
+  },
+
+  notify: function(type) {
+    if(Game.world.tutorial && Game.world.tutorial.length) {
+      var t = Game.world.tutorial[0];
+      if(t.dismiss.type.toLowerCase() == "on" + type.toLowerCase()) {
+        Game.world.tutorial.splice(0, 1);
       }
-    };
+    }
+
+    switch(type) {
+      case "levelComplete":
+        Game.levelComplete();
+        break;
+
+      case "death":
+        Game.handleDeath();
+        break;
+    }
+  },
+
+  handleDeath: function() {
+    Game.gameState = Game.GAME_STATE_PLAYER_DEAD;
+    window.setTimeout(Game.loadLevel, 3000, Game.worldIndex);
+  },
+
+  loadLevel: function(level) {
+    Game.clearInput();
+    Game.worldIndex = level || 0;
+    Game.world = World.loadWorld(Game.worldIndex);
+    Game.player = Player.loadPlayer(Game.world.playerStart);
+    Game.player.teleports = 0;
+    Game.gameState = Game.GAME_STATE_PLAYING;
+  },
+
+  clearScreen: function() {
+    Game.p.background(55);
+  },
+
+  levelComplete: function() {
+    Game.assets.sndLevelCompleted.play();
+    Game.gameState = Game.GAME_STATE_LEVEL_COMPLETE;
+    Game.levelCompleteTimer = 3000;
+    Game.levelCompleteText = ["Good Job!", "Great!", "Excellent!", "Well Done!", "Awesome!"][Game.p.floor(Game.p.random(5))];
+  },
+
+  drawStart: function() {
+    Game.clearScreen();
+    Game.p.textAlign(Game.p.CENTER, Game.p.CENTER);
+    Game.p.fill(Game.p.color(255));
+
+    Game.p.textSize(40);
+    Game.p.text("Connected Worlds", Game.halfWidth, Game.halfHeight - 40);
+
+    Game.p.textSize(18);
+    Game.p.text("Staring Switch", Game.halfWidth, Game.halfHeight + 20);
+
+    Game.p.textSize(12);
+    Game.p.text("Completed for Ludum Dare #30/48", Game.halfWidth, Game.halfHeight + 50);
+
+    Game.p.textSize(18);
+    Game.p.text("Press Any Key", Game.halfWidth, Game.halfHeight + 140);
+
+    Game.p.noFill();
+    Game.p.stroke(255);
+    Game.p.rect(Game.halfWidth - 100, Game.halfHeight + 140 - 20, 200, 40, 18);
+  },
+
+  drawGame: function() {
+    Game.clearScreen();
+    Game.p.image(Game.assets.sky, 0, 0, Game.p.width, Game.halfHeight);
+    Game.p.image(Game.assets.darSky, 0, Game.halfHeight, Game.p.width, Game.halfHeight);
+    Game.p.pushMatrix();
+    Game.p.translate(0, 10);
+    Game.world.draw();
+    Game.player.draw();
+    Game.p.popMatrix()
+  },
+
+  drawHUD: function() {
+    //Game.p.fill(255);
+    //Game.p.textSize(12);
+    //Game.p.textAlign(Game.p.LEFT, Game.p.TOP);
+    //Game.p.text("TIME: " + Game.p.floor(Game.worldTime / 1000), Game.p.width - 120, 20);
+    //Game.p.text("TELEPORTS: " + Game.player.teleports, Game.p.width - 120, 40);
+
+    if(Game.world.tutorial && Game.world.tutorial.length) {
+      var t = Game.world.tutorial[0];
+      Game.drawDialog(t.text.replace(/\[player\]/g, Game.player.name));
+    }
+
+    for(var i = 0; i < Game.player.life; i++) {
+      Game.context.drawImage(Game.assets.tiles, 5 * 32, 0, 32, 32, i * 32, 0, 32, 32);
+    }
+  },
+
+  drawDialog: function(text) {
+    Game.p.stroke(255);
+    Game.p.fill(0, 0, 0, 160);
+    Game.p.rect(200, Game.halfHeight - 50, Game.p.width - 400, 100, 8);
+    Game.p.fill(255);
+    Game.p.textAlign(Game.p.CENTER, Game.p.CENTER);
+    Game.p.text(text, Game.halfWidth, Game.halfHeight);
+  },
+
+  drawLevelComplete: function() {
+    Game.drawDialog(Game.levelCompleteText);
+  },
+
+  drawLevelStats: function() {
+    Game.drawDialog("Press Any Key");
+  },
+
+  drawCredits: function() {
+    Game.p.background(0);
+    Game.drawDialog("THAT'S ALL FOLKS!\nTHANKS FOR PLAYING!\n\nFeedback: @wesleyjohnson");
+  },
+
+  draw: function() {
+    // Calculate gameTime
+    var cms = Game.p.millis();
+    var gameTime = (cms - Game.ms) / 1000;
+    Game.ms = cms;
+
+    // Update
+    Game.update(gameTime);
+
+    // Render
+    switch(Game.gameState) {
+      case Game.GAME_STATE_START:
+        Game.drawStart();
+        break;
+
+      case Game.GAME_STATE_PLAYING:
+      case Game.GAME_STATE_PAUSED:
+      case Game.GAME_STATE_LEVEL_COMPLETE:
+        Game.drawGame();
+        Game.drawHUD();
+        if(Game.gameState == Game.GAME_STATE_LEVEL_COMPLETE) {
+          Game.drawLevelComplete();
+        }
+        break;
+
+      case Game.GAME_STATE_PLAYER_DEAD:
+        Game.drawGame();
+        Game.drawDialog("OUCH!\nYou'll have to start over.");
+        break;
+
+      case Game.GAME_STATE_LEVEL_STATS:
+        Game.drawLevelStats();
+        break;
+
+      case Game.GAME_STATE_CREDITS:
+        Game.drawCredits();
+        break;
+    }
   }
 }
--- js/player.js
+++ js/player.js
@@ -9,9 +9,15 @@ var Player = function(x, y, world) {
   this.yVel = 0;
   this.image = 0;
   this.imageTimer = 0;
-  this.leftWalkSequence = [0, 1, 0, 2];
-  this.rightWalkSequence = [3, 4, 3, 5];
+  this.leftWalkSequence = [4,1,0,1,4,2,3,2];
+  this.rightWalkSequence = [5,8,9,8,5,7,6,7];
   this.sequence = this.rightWalkSequence;
+
+  this.teleports = 0;
+  this.life = 3;
+  this.hurtTimer = 0;
+
+  this.name = "Switch";
 
   this.getTile = function(x, y) {
     if(!arguments.length) {
@@ -27,18 +33,61 @@ var Player = function(x, y, world) {
     var newWorld = this.world == "norm" ? "alt" : "norm";
     if(!Game.world.isColliding(newWorld, this.x, this.y)) {
       this.world = newWorld;
+      this.teleports += 1;
+      Game.assets.sndTeleport.play();
+      Game.notify("teleport");
     }
   }
 
   this.isGrounded = function() {
-    return Game.world.isColliding(this.world, this.x, this.y + this.height);
+    return Game.world.isColliding(this.world, this.x, this.y + this.height) && this.yVel == 0;
   }
 
   this.jump = function() {
-    this.yVel -= 240;
+    this.yVel = -275;
+    Game.notify("jump");
+    Game.assets.sndJump.play();
+  }
+
+  this.hurt = function(dmg, bounceDir) {
+    // Can't be damaged again too quickly
+    if(!this.hurtTimer > 0) {
+      this.life -= dmg;
+      this.hurtTimer = 2000;
+    }
+
+    if(this.life == 0) {
+      Game.notify("death");
+      return;
+    }
+
+    if(bounceDir >= 0) {
+      switch(bounceDir) {
+        case 0:
+          break;
+
+        case 1:
+          this.yVel = -275;
+          break;
+
+        case 2:
+          break;
+
+        case 3:
+          break;
+      }
+    }
   }
 
   this.update = function(gameTime) {
+    // Update hurt hurTimer
+    if(this.hurtTimer > 0) {
+      this.hurtTimer -= 1000 * gameTime;
+      if(this.hurtTimer <= 0) {
+        this.hurtTimer = 0;
+      }
+    }
+
     // Update gravity if we're not on the ground
     if(!this.isGrounded()) {
       this.yVel += 9.8;
@@ -53,7 +102,7 @@ var Player = function(x, y, world) {
       this.sequence = this.xVel < 0 ? this.leftWalkSequence : this.rightWalkSequence;
       if(this.imageTimer <= 0) {
         this.image = (this.image + 1) % this.sequence.length;
-        this.imageTimer = 160;
+        this.imageTimer = 80;
       }
     }
     else {
@@ -92,14 +141,14 @@ var Player = function(x, y, world) {
     // Only apply movement if we're not colliding
     if(newX > this.x) {
       if(Game.world.isColliding(this.world, newX + (this.width >> 1) + 1, this.y)) {
-        this.xVel = 0;
+        //this.xVel = 0;
         newX = this.x;
       }
     }
     else if(newX < this.x)
     {
       if(Game.world.isColliding(this.world, newX - (this.width >> 1) - 1, this.y)) {
-        this.xVel = 0;
+        //this.xVel = 0;
         newX = this.x;
       }
     }
@@ -131,19 +180,27 @@ var Player = function(x, y, world) {
   };
 
   this.draw = function() {
-    Game.p.noStroke();
-    Game.p.fill(255);
-    //Game.p.ellipse(this.x, this.getPlayerY(), this.width, this.height);
-
-    Game.p.stroke(255);
-    Game.p.noFill();
-    Game.p.ellipse(this.x, this.getPlayerY(true), this.width, this.height);
-    Game.p.fill(255);
-    Game.p.text(this.xVel, this.x  + 50, this.y);
-
+    var outter = Game.p.random(3, 10);
+    var lineLen = Game.p.random(10, this.width);
     Game.p.pushMatrix();
-    Game.p.blend(Game.assets.guy, this.sequence[this.image] * 32, 0, 32, 32, this.x - 16, this.getPlayerY(), this.width, this.height, Game.p.BLEND );
-    Game.p.popMatrix()
+    Game.p.translate(this.x, this.getPlayerY(true));
+    Game.p.rotate(Game.p.random(360));
+    Game.p.noStroke();
+    Game.p.fill(255, 255, 255, 50);
+    Game.p.ellipse(0, 0, 5, 5);
+    Game.p.stroke(255, 255, 255, 50);
+    Game.p.noFill();
+    Game.p.ellipse(0, 0, outter, outter);
+    Game.p.fill(255, 255, 255, 50);
+    Game.p.rect(-(lineLen >> 1), -1, lineLen, 1);
+    Game.p.rect(-1, -(lineLen >> 1), 1, lineLen);
+    Game.p.popMatrix();
+
+    //Game.p.pushMatrix();
+    if(this.hurtTimer == 0 || Game.p.floor(this.hurtTimer / 250) % 2 == 0) {
+      Game.context.drawImage(Game.assets.tiles, this.sequence[this.image] * 32, 15 * 32, 32, 32, this.x - 16, this.getPlayerY() - 12, this.width, this.height);
+    }
+    //Game.p.popMatrix()
 
   }
 };
